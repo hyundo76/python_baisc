18fc73df15384fc8c0724cb96f77d9780ad49b77
hyundo76 <qkrguseh99@gmail.com>
2025-03-06 17:40:36 +0900

    문자열

---
+++ 03_문자열.py
@@ -0,0 +1,16 @@
+test01 = "문자열"
+test02 = '문자열'
+test03 = "'dddd'"
+
+data1 = "(i eat %d apples, %s)" 
+print(data1 %(3, "ggg"))
+
+
+txt01 = "I eat {0:0.2f} apples, {1}"
+print(txt01.format(3, 'p'))
+
+name = "hyun"
+age = 24
+txt04 = f'my name is {name:^20}, age is {age:.2f}'
+print(txt04)
+
